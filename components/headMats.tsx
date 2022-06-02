d872b4589a0a784b2d05fb3fdb641de17c591ae0
Harris02 <86095684+Harris02@users.noreply.github.com>
2022-06-02 17:17:20 +0700

    Update headMats.tsx

--- components/headMats.tsx
+++ components/headMats.tsx
@@ -5,7 +5,7 @@ export default function HeadMats({title,img}:any){
     return(
     <div>
       <Head>
-        <link rel="shortcut icon" href={img}/>
+        <link rel="shortcut alt="atasan" icon" href={img}/>
         <title>{title}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
